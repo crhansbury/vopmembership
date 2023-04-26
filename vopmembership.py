5ee4e37a1803c74b92891c648dd245578ff9da96
Cass <crhansbury@gmail.com>
2023-04-26 15:16:30 -0400

    Changed functions so now they output lists instead of dictionaries.
    The functions `create_active_dictionary` and `create_inactive_dictionary`
    are changed to `create_active_members` and `create_inactive_members`.
    The functions now return a list of objects of the Member class,
    instead of a dictionary whose values are a list of Member objects.
    This is an experiment to see if it simplifies the way one iterates
    over the Member objects and calls their attributes. I found that I
    wasn't using the key in the dictionary at all, and that making a
    dict was unneccessary.

--- vopmembership.py
+++ vopmembership.py
@@ -4,14 +4,14 @@ from mapping import MEMBER_ID, MEMBER_FIRST, MEMBER_LAST, \
     SECTION, PRONOUNS, ROLE, \
     ADDRESS, CITY, STATE, ZIP, PHONE, EMAIL 
 
-def create_active_dictionary(file):
-    """Creates a dictionary using the information from the spreadsheet
+def create_active_members(file):
+    """Creates a list using the information from the spreadsheet
     of all active members. The key is the member id, and the value is 
     an instance of the Member dataclass, which includes all of the information
     from the columns in the spreadsheet."""
     workbook = load_workbook(filename=file)
     active_sheet = workbook["Active Members"]
-    active_members = {}
+    active_members = []
     for row in active_sheet.iter_rows(min_row=2, values_only=True):
         active_member = Member(id=row[MEMBER_ID],
                                first_name=row[MEMBER_FIRST],
@@ -25,15 +25,15 @@ def create_active_dictionary(file):
                                state=row[STATE],
                                zip=row[ZIP],
                                phone=row[PHONE])
-        active_members[active_member.id] = active_member
+        active_members.append(active_member)
     return active_members
 
-def create_inactive_dictionary(file):
-    """Creates a dictionary using the information from the spreadsheet
+def create_inactive_members(file):
+    """Creates a list using the information from the spreadsheet
     of all inactive members."""
     workbook = load_workbook(filename=file)
     inactive_sheet = workbook["Inactive Members"]
-    inactive_members = {}
+    inactive_members = []
     for row in inactive_sheet.iter_rows(min_row=2, values_only=True):
         inactive_member = Member(id=row[MEMBER_ID],
                                first_name=row[MEMBER_FIRST],
@@ -47,7 +47,7 @@ def create_inactive_dictionary(file):
                                state=row[STATE],
                                zip=row[ZIP],
                                phone=row[PHONE])
-        inactive_members[inactive_member.id] = inactive_member
+        inactive_members.append(inactive_member)
     return inactive_members
 
 def main():
@@ -56,5 +56,6 @@ def main():
     make a member inactive, or query the spreadsheet for information"""
     pass
 
-inactive_dict = create_inactive_dictionary("vopmembership_data.xlsx")
-print(inactive_dict)
+active_member = create_active_members("vopmembership_data.xlsx")
+for member in active_member:
+    print(member.pronouns)
