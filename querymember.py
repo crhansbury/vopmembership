8dd069138110fb7a35de9115b530cda04c3f1ef3
Cass <crhansbury@gmail.com>
2023-04-26 15:42:11 -0400

    Filled in the `query_member()` function.
    `query_member()` searches the list of Member objects for a certain
    attribute. It takes two parameters, the filename and the attribute
    you wish to search for. It returns a list of all of the attribute
    values from the search.
    
    I'm thinking to use this function to help create the member ID,
    possibly having it iterate through this list to find all the
    ID numbers that already exist and creating a new number that is
    in sequence.

--- querymember.py
+++ querymember.py
@@ -1,4 +1,20 @@
-def query_member():
-    """Prompts the user to input a search for information in the spreadsheet,
-    and searches the spreadsheet for that member's information."""
-    pass
+from vopmembership import create_active_members
+
+def query_member(file, attribute):
+    """Searches the list of Member objects for a certain attribute, returning
+    a list of the attributes."""
+    active_members = create_active_members(file)
+    member_list = []
+    for member in active_members:
+        attr_value = getattr(member, attribute)
+        member_list.append(attr_value)
+    return member_list
+    
+value = query_member("vopmembership_data.xlsx", "first_name")
+print(value)
+
+
+    
+
+# Could use this to look up the members in the dictionary with the other
+# functions as well, such as updatemember and move member.
