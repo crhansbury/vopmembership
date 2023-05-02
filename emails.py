873501b1e6a5ef5cfd9b6f55372145aa7ca427d2
Cass <crhansbury@gmail.com>
2023-05-02 14:42:03 -0400

    Created the `send_email()` function.
    Sends an email from vopmembershiptest@gmail.com to the recipient
    defined in the parameters. It takes three parameters -- recipient,
    email subject, and email body.
    
    It uses SMTP over TLS to securely send the email. The password is
    hidden in an environment variable on my local machine.

--- emails.py
+++ emails.py
@@ -1,19 +1,44 @@
+from email.message import EmailMessage
+import smtplib
+import os
+
 def mailing_list():
     """Updates the mailing list for each email group. The email groups 
     include section groups, role groups, and all singers."""
     # Not sure about this function, might not use it.
 
-def send_email(email, email_file):
+def send_email(reciever_email, subject, body):
     """Sends an email to a member in the spreadsheet. Planning to use for 
     update confirmations, semesterly prompts for updates, 
     new member welcome email"""
-    # The body of the email can be stored in a file template, which a separate 
-    # function opens and then appends appropriately, and this function puts into
-    #  the email body variable. 
-    pass
+    
+    sender_email = "vopmembershiptest@gmail.com"
+    password = os.environ.get("VOP_PASSWORD")
+    
+    #Create an instance of the EmailMessage class to generate the email
+    em = EmailMessage()
+    em['From'] = sender_email
+    em['To'] = reciever_email
+    em['Subject'] = subject
+    em.set_content(body)
+    
+    # Log in to sender_email and define SMTP server
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587 # SMTP over TLS
+    
+    # Connect to email server using TLS
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.login(sender_email, password)
 
-def new_member_email_template(file, email):
+    # Send the email
+    server.sendmail(sender_email, reciever_email, em.as_string())
+    server.quit()
+
+def new_member_email_body(file, receiver_email):
     """Fills in an existing template for a new member email with the new 
     member's name, section, and section leader, creating a new file with
-    the body of the email."""
+    the body of the email and returning the contents of that file as a str."""
     pass
+
